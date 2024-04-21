f3a12f84dfb5a7dc8875b35ef5028023f1605fc7
Kartik Dixit <kartikdixit2468@gmail.com>
2024-04-21 17:48:48 +0530

    resume linked with download button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const port = 3500;
 
 const app = express();
@@ -7,13 +8,35 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
 
-        res.send("App Running successfully!")
+    const options = {
+        root: path.join(__dirname)
+    };
+    const fileName = 'views/index.html';
+    
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Files rendered Successfully:', fileName);
+        }
+    });
+
+});
+
+app.get('/resume-download', (req, res) => {
+
+    const resume_path = 'public/files/Kartik Dixit Resume.pdf';    
+    res.download(resume_path, (err) => {
+        if (err) {
+            res.send(`An unknown Error occured, Please try again later! - ${err}`);
+        }
+});
 
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
     console.log(`Link to application - http://localhost:${port}/`)
-})
+});
